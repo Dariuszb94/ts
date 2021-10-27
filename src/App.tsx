202bbdc9b77fe094d6737cbb5a03d2d4fc4e25e6
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-27 22:04:36 +0200

    heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-
+function Heading({title}:{title:string;}){
+return <h1>{title}</h1>
+}
 function App() {
   return (
     <div className="App">
+      <Heading title="Hello there"/>
     </div>
   );
 }
