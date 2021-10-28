2e262ed6a0ad0f71f0da9dfa512aacc70ff1d2a1
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-28 21:37:00 +0200

    heading with content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { ReactNode, ReactElement } from 'react';
 import './App.css';
 function Heading({title}:{title:string;}){
 return <h1>{title}</h1>
 }
+function HeadingwithContent({children}:{children:ReactNode}){
+  return <h1>{children}</h1>
+  }
+  const defaultContainerProps={
+    heading:<strong>My heading</strong>,
+  }
+  function Container({
+    heading, 
+    children,
+  }:{children:ReactNode}&typeof defaultContainerProps):ReactElement{
+    return <div><h1>{heading}</h1>{children}</div>
+    }
+    Container.defaultContainerProps=defaultContainerProps
 function App() {
   return (
     <div className="App">
       <Heading title="Hello there"/>
+      <HeadingwithContent>Hi!</HeadingwithContent>
+      {/* <Container>Foo</Container> */}
     </div>
   );
 }
